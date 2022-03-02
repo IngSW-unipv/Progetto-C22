30a8b9ce116a9b7f97864e3b06fe72a632d5ab8e
grecyaDangio <grecya.dangio01@universitadipavia.it>
2022-03-02 09:07:00 +0100

    modifiche per togliere i warnings

--- src/test/CinemaTest.java
+++ src/test/CinemaTest.java
@@ -1,12 +1,7 @@
 package test;
 
-import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertFalse; 
 import static org.junit.Assert.assertTrue;
-
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 
 import database.MySQLConnection;
