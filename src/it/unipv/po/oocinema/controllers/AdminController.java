4ae1462ab0bf523ae5255ab797b4d47a26579782
matteoragni01 <matteo.ragni01@universitadipavia.it>
2022-04-01 11:50:43 +0200

    adminController

---
+++ src/it/unipv/po/oocinema/controllers/AdminController.java
@@ -0,0 +1,92 @@
+package it.unipv.po.oocinema.controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.control.ToggleButton;
+import javafx.scene.input.MouseEvent;
+
+public class AdminController {
+
+    @FXML
+    private ToggleButton aggiungi;
+
+    @FXML
+    private Label cassa;
+
+    @FXML
+    private TextArea descrizione;
+
+    @FXML
+    private TextField durata;
+
+    @FXML
+    private Label esci;
+
+    @FXML
+    private Label film;
+
+    @FXML
+    private TextField genere;
+
+    @FXML
+    private Label home;
+
+    @FXML
+    private Button locandina;
+
+    @FXML
+    private Label proiezione;
+
+    @FXML
+    private TextField regista;
+
+    @FXML
+    private TextField titolo;
+
+    @FXML
+    private Button trailer;
+
+    @FXML
+    void aggiungiFilm(MouseEvent event) {
+
+    }
+
+    @FXML
+    void caricaLocandina(MouseEvent event) {
+
+    }
+
+    @FXML
+    void caricaTrailer(MouseEvent event) {
+
+    }
+
+    @FXML
+    void cassa(MouseEvent event) {
+
+    }
+
+    @FXML
+    void esci(MouseEvent event) {
+
+    }
+
+    @FXML
+    void film(MouseEvent event) {
+
+    }
+
+    @FXML
+    void home(MouseEvent event) {
+
+    }
+
+    @FXML
+    void proiezione(MouseEvent event) {
+
+    }
+
+}
